1686a74c76cf28888d2dcbf71c6780a2b2e597af
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-07 19:46:55 +0600

    adding solution 4 & 5

--- src/solutions.ts
+++ src/solutions.ts
@@ -12,4 +12,44 @@ function filterEvenNumbers(numbers: number[]) : number[]{
 function reverseString(str : string) : string{
     return str.split("").reverse().join("");
 }
-console.log(reverseString("typescript"));
+// console.log(reverseString("typescript"));
+
+
+
+// Problem-3
+type StringOrNumber = string | number;
+function checkType(value : StringOrNumber) : "String" | "Number" {
+    if( typeof value === "string"){
+        return "String";
+    }else{
+        return "Number";
+    }
+}
+
+
+// console.log(checkType("Sara"));
+
+
+
+// Problem-4
+function getProperty(obj : any, key : string){
+    return obj[key];
+}
+const user = { id : 1, name : "John Doe", age : 21};
+// console.log(getProperty(user, "name"));
+
+
+//Problem-5
+interface Book{
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+function toggleReadStatus(book: Book) : Book & { isRead : boolean}{
+    return {...book, isRead: true};
+}
+const myBook = { title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024,
+  isRead: true};
+//   console.log(toggleReadStatus(myBook));
